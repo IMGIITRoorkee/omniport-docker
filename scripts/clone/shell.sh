497cde02d2c52eac1a01bb5393a08df9918132eb
Dhruv Bhanushali <dhruv_b@live.com>
2018-04-29 17:14:29 +0530

    Add another cloning script to clone the shell alongs the lines of the script to clone the core

---
+++ scripts/clone/shell.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Enter the Omniport code directory
+cd omniport
+
+# Clone the Omniport shell repository from the remote
+git clone https://bitbucket.org/me-me-big-boy/omniport-shell.git shell
