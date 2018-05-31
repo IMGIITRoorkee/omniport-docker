57df65f82d74110bc7d2594f1e3604168012dbf5
Dhruv Bhanushali <dhruv_b@live.com>
2018-05-31 11:20:10 +0530

    Give the containers names and add the rm flag to delete the containers after use

--- scripts/run/django.sh
+++ scripts/run/django.sh
@@ -3,15 +3,20 @@
 # Get the current working directory
 PWD=$(pwd)
 
+# Get a name for the container, defaults to django-dev
+NAME=${1:-'django-dev'}
+
 # Get the interface and port to bind to, defaults to 0.0.0.0:8000
-BIND=${1:-'0.0.0.0:8000'}
+BIND=${2:-'0.0.0.0:8000'}
 
 # Run the container with the specified settings
 # Start the DJ!
 docker run \
 	--tty \
 	--interactive \
+	--rm \
 	--net="host" \
+	--name=$NAME \
 	--env SITE_ID=0 \
 	--mount type=bind,src=$PWD/omniport,dst=/omniport \
 	--mount type=bind,src=$PWD/configuration,dst=/omniport/configuration \
--- scripts/run/logs.sh
+++ scripts/run/logs.sh
@@ -3,12 +3,17 @@
 # Get the current working directory
 PWD=$(pwd)
 
+# Get a name for the container, defaults to logs-prod
+NAME=${1:-'logs-prod'}
+
 # Run the container with the specified settings
 # Examine logs!
 docker run \
 	--tty \
 	--interactive \
+	--rm \
 	--net="host" \
+	--name=$NAME \
 	--mount type=volume,src=omniportdocker_gunicorn_logs,dst=/gunicorn_logs \
     --mount type=volume,src=omniportdocker_daphne_logs,dst=/daphne_logs \
 	--mount type=volume,src=omniportdocker_nginx_logs,dst=/nginx_logs \
