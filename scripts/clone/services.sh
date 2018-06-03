9bca753b199fba7e508cce3cb265827a62bd6ea5
Dhruv Bhanushali <dhruv_b@live.com>
2018-06-03 17:50:40 +0530

    Add a script to clone services, along the lines of the other two cloning scripts

---
+++ scripts/clone/services.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Enter the Omniport services directory
+cd omniport/services
+
+# Clone the Omniport core repository from the remote
+git clone https://github.com/IMGIITRoorkee/omniport-service-bootstrap.git bootstrap
