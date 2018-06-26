4bfde1d62de0b47aa844c83ae2002676bec3ac33
Dhruv Bhanushali <dhruv_b@live.com>
2018-06-27 01:50:08 +0530

    Fix the condition of logging in the various containers

--- django/gunicorn_config.py
+++ django/gunicorn_config.py
@@ -5,5 +5,5 @@ bind = '0.0.0.0:8000'
 reload = True
 
 site_id = int(os.getenv('SITE_ID', '0'))
-accesslog = f'/gunicorn_logs/{site_id}-access.log'
-errorlog = f'/gunicorn_logs/{site_id}-error.log'
+accesslog = f'/web_server_logs/gunicorn_logs/{site_id}-access.log'
+errorlog = f'/web_server_logs/gunicorn_logs/{site_id}-error.log'
