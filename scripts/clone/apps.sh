14d61efbf089832172a746cf0854b46ba84af8bc
Dhruv Bhanushali <dhruv_b@live.com>
2018-07-26 21:50:20 +0530

    Clean up the Bash script

--- scripts/clone/apps.sh
+++ scripts/clone/apps.sh
@@ -2,9 +2,10 @@
 
 urlencode() {
     # urlencode <string>
-    old_lc_collate=$LC_COLLATE
-    LC_COLLATE=C
     
+    old_lc_collate=$LC_COLLATE
+    
+    LC_COLLATE=C
     local length="${#1}"
     for (( i = 0; i < length; i++ )); do
         local c="${1:i:1}"
