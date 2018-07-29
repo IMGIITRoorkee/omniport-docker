0e811cf1a4dcb059481e793360297ab5249a91c5
Dhruv Bhanushali <dhruv_b@live.com>
2018-07-29 05:33:42 +0530

    Set the name to current timestamp and remove network connectivity which is not needed

--- scripts/run/logs.sh
+++ scripts/run/logs.sh
@@ -4,7 +4,7 @@
 PWD=$(pwd)
 
 # Get a name for the container, defaults to logs-prod
-NAME=${1:-'logs-prod'}
+NAME=$(date +"%s")
 
 # Run the container with the specified settings
 # Examine logs!
@@ -12,7 +12,6 @@ docker run \
 	--tty \
 	--interactive \
 	--rm \
-	--network=omniport-docker_default \
 	--mount type=volume,src=omniport-docker_reverse_proxy_logs,dst=/reverse_proxy_logs \
 	--mount type=volume,src=omniport-docker_web_server_logs,dst=/web_server_logs \
 	--name=$NAME \
