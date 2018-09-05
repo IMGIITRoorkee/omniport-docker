9218ea0f03365bbc1e3bf8b5d5347f5ba9a2de7e
Gaurav Kumar <gaurav.kumar9825@gmail.com>
2018-09-06 03:33:25 +0530

    Fix punctuation

--- scripts/build/django.sh
+++ scripts/build/django.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-# Copy the requirements file from the codebase into the Django Docker folder
+# Copy the requirements.txt file from the codebase into the Django Docker folder
 cp codebase/omniport-backend/omniport/requirements.txt django/
 
 # Enter the Django Docker folder
 cd django/
 
-# Build the container from the django folder and tag it
+# Build the container from the Django folder and tag it
 TIMESTAMP=$(date +"%s")
 docker build --tag omniport-django:${TIMESTAMP} --tag omniport-django:latest .
 
-# Remove the requirements file after it has served its purpose
+# Remove the requirements.txt file after it has served its purpose
 rm requirements.txt
