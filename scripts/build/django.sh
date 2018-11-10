ad11e25cea64b170176254f4f6646d8a884ec24c
Dhruv Bhanushali <dhruv_b@live.com>
2018-11-10 13:56:00 +0530

    Make changes to the image build according to the splitting of requirements in omniport-backend

--- scripts/build/django.sh
+++ scripts/build/django.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
-# Copy the requirements.txt file from the codebase into the Django Docker folder
-cp codebase/omniport-backend/omniport/requirements.txt django/
+# Copy the prod-requirements.txt file from the codebase into the Django Docker folder
+cp codebase/omniport-backend/omniport/prod-requirements.txt django/
+
+read -p "Add PyPI dependencies for developer tools? (y/N): " DEV_TOOLS
+if [ $DEV_TOOLS == 'Y' -o $DEV_TOOLS == 'y' ]; then
+    # Copy the dev-requirements.txt file from the codebase into the Django Docker folder
+    cp codebase/omniport-backend/omniport/dev-requirements.txt django/
+else
+    # Ensure a blank dev-requirements.txt file exists
+    echo '' > django/dev-requirements.txt
+fi
 
 # Enter the Django Docker folder
 cd django/
@@ -10,5 +19,5 @@ cd django/
 TIMESTAMP=$(date +"%s")
 docker build --tag omniport-django:${TIMESTAMP} --tag omniport-django:latest .
 
-# Remove the requirements.txt file after it has served its purpose
-rm requirements.txt
+# Remove the requirement files after they have served their purpose
+rm prod-requirements.txt dev-requirements.txt
