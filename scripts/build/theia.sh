c723bfd0af4b773bd84fb20e0e72ca8051b52e9a
Dhruv Bhanushali <dhruv_b@live.com>
2019-01-12 20:00:17 +0530

    Remove the ghost of Theia from the repository

--- scripts/build/theia.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Enter the Thiea Docker folder
-cd theia/
-
-# Build the container from theia folder and tag it
-TIMESTAMP=$(date +"%s")
-docker build --tag omniport-theia:${TIMESTAMP} --tag omniport-theia:latest .
