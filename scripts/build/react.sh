b9be882efb41304030873ebd81ad0422a4a308e1
Dhruv Bhanushali <dhruv_b@live.com>
2019-01-23 00:48:11 +0530

    Dockerise the React-based frontend of Omniport

--- scripts/build/react.sh
+++ scripts/build/react.sh
@@ -1,14 +1,37 @@
 #!/bin/sh
 
-# Copy the package.json file from the codebase into the React Docker folder
-cp codebase/omniport-frontend/package.json react/
+# Copy the yarn.lock file from the codebase into the React Docker folder
+cp codebase/omniport-frontend/omniport/package.json react/
+cp codebase/omniport-frontend/omniport/yarn.lock    react/
+
+read -p "Add npm dependencies for developer tools? (y/N): " DEV_TOOLS
+if [ $DEV_TOOLS == 'Y' -o $DEV_TOOLS == 'y' ]; then
+    # Set the build argument IMAGETYPE to blank
+    IMAGETYPE=''
+
+    # Assign a tag of dev-latest for usecases that mandate development
+    TAG='dev-latest'
+else
+    # Set the build argument IMAGETYPE to --prod
+    IMAGETYPE='--prod'
+
+    # Assign a tag of prod-latest for usecases that mandate production
+    TAG='prod-latest'
+fi
 
 # Enter the React Docker folder
 cd react/
 
 # Build the container from the React folder and tag it
 TIMESTAMP=$(date +"%s")
-docker build --tag omniport-react:${TIMESTAMP} --tag omniport-react:latest .
 
-# Remove the package.json file after it has served its purpose
+docker build \
+    --build-arg IMAGETYPE=${IMAGETYPE} \
+    --tag omniport-react:${TIMESTAMP} \
+    --tag omniport-react:${TAG} \
+    --tag omniport-react:latest \
+    .
+
+# Remove the yarn.lock file after it has served its purpose
 rm package.json
+rm yarn.lock
