0b57b5819bf99ca4da6405b4cb513a56dbb75e47
Dhruv Bhanushali <dhruv_b@live.com>
2019-02-03 10:06:55 +0530

    Create a generalised system for the generation of NGINX .conf files

--- scripts/build/nginx.sh
+++ scripts/build/nginx.sh
@@ -8,12 +8,12 @@ if [ $REBUILD == 'Y' -o $REBUILD == 'y' ]; then
     read -p "Enter the intranet-side domain as 'omniport.intranet': " INTRANET_DOMAIN
     read -p "Enter the Internet-side domain as 'omniport.internet': " INTERNET_DOMAIN
 
+    # Choose whether to enable SSL in the NGINX conf
     read -p "Enable HTTPS? [y/N]: " HTTPS
-
     if [ $HTTPS == 'Y' -o $HTTPS == 'y' ]; then
         MAIN_PORT='443 ssl'   # 'ssl' is essential here
-        REDIRECT_INTRANET='include conf\.d\/includes\/01-http_redirect\.conf;'
-        REDIRECT_INTERNET='include conf\.d\/includes\/02-http_redirect\.conf;'
+        REDIRECT_INTRANET='include conf\.d\/includes\/01-redirect\.conf;'
+        REDIRECT_INTERNET='include conf\.d\/includes\/02-redirect\.conf;'
         ENABLE_SSL='include conf\.d\/includes\/ssl\.conf;'
     else
         MAIN_PORT='80'
@@ -26,50 +26,78 @@ if [ $REBUILD == 'Y' -o $REBUILD == 'y' ]; then
     cd conf.d/
 
     # Remove pre-existing .conf files
-    rm 01-intranet.conf 02-internet.conf
-    rm includes/01-http_redirect.conf includes/02-http_redirect.conf
+    rm 0*.conf
+    rm includes/0*.conf
 
     # Perform text substitution to generate the new .conf files
-    printf "Writing intranet .conf file... "
-    cp stencils/01-intranet.conf ./01-intranet.conf
-    sed -i "s/\[\[intranet_domain\]\]/${INTRANET_DOMAIN}/g" 01-intranet.conf
-    sed -i "s/\[\[main_port\]\]/${MAIN_PORT}/g" 01-intranet.conf
+    printf "Writing intranet application file... "
+    cp stencils/application.conf includes/01-application.conf
+    sed -i "s/\[\[side\]\]/intranet/g" includes/01-application.conf
+    printf "done\n"
+
+    printf "Writing intranet http_redirect file... "
+    cp stencils/redirect.conf includes/01-redirect.conf
+    sed -i "s/\[\[domain\]\]/${INTRANET_DOMAIN}/g" includes/01-redirect.conf
+    printf "done\n"
+
+    printf "Writing intranet logging file... "
+    cp stencils/logging.conf includes/01-logging.conf
+    sed -i "s/\[\[side\]\]/intranet/g" includes/01-logging.conf
+    printf "done\n"
+
+    printf "Writing intranet upstreams file... "
+    cp stencils/upstreams.conf includes/01-upstreams.conf
+    sed -i "s/\[\[side\]\]/intranet/g" includes/01-upstreams.conf
+    printf "done\n"
+
+    printf "Writing main intranet .conf file... "
+    cp stencils/main.conf 01-intranet.conf
+    sed -i "s/\[\[side\]\]/intranet/g" 01-intranet.conf
+    sed -i "s/\[\[id\]\]/01/g" 01-intranet.conf
     sed -i "s/\[\[redirect\]\]/${REDIRECT_INTRANET}/g" 01-intranet.conf
+    sed -i "s/\[\[main_port\]\]/${MAIN_PORT}/g" 01-intranet.conf
+    sed -i "s/\[\[domain\]\]/${INTRANET_DOMAIN}/g" 01-intranet.conf
     sed -i "s/\[\[enable_ssl\]\]/${ENABLE_SSL}/g" 01-intranet.conf
     printf "done\n"
 
-    printf "Writing intranet redirect file... "
-    cp stencils/01-http_redirect.conf ./includes/01-http_redirect.conf
-    sed -i "s/\[\[intranet_domain\]\]/${INTRANET_DOMAIN}/g" ./includes/01-http_redirect.conf
+    printf "Writing Internet application file... "
+    cp stencils/application.conf includes/02-application.conf
+    sed -i "s/\[\[side\]\]/internet/g" includes/02-application.conf
     printf "done\n"
 
-    printf "Writing Internet .conf file... "
-    cp stencils/02-internet.conf ./02-internet.conf
-    sed -i "s/\[\[internet_domain\]\]/${INTERNET_DOMAIN}/g" 02-internet.conf
-    sed -i "s/\[\[main_port\]\]/${MAIN_PORT}/g" 02-internet.conf
+    printf "Writing Internet http_redirect file... "
+    cp stencils/redirect.conf includes/02-redirect.conf
+    sed -i "s/\[\[domain\]\]/${INTERNET_DOMAIN}/g" includes/02-redirect.conf
+    printf "done\n"
+
+    printf "Writing Internet logging file... "
+    cp stencils/logging.conf includes/02-logging.conf
+    sed -i "s/\[\[side\]\]/internet/g" includes/02-logging.conf
+    printf "done\n"
+
+    printf "Writing Internet upstreams file... "
+    cp stencils/upstreams.conf includes/02-upstreams.conf
+    sed -i "s/\[\[side\]\]/internet/g" includes/02-upstreams.conf
+    printf "done\n"
+
+    printf "Writing main Internet .conf file... "
+    cp stencils/main.conf 02-internet.conf
+    sed -i "s/\[\[side\]\]/internet/g" 02-internet.conf
+    sed -i "s/\[\[id\]\]/02/g" 02-internet.conf
     sed -i "s/\[\[redirect\]\]/${REDIRECT_INTERNET}/g" 02-internet.conf
+    sed -i "s/\[\[main_port\]\]/${MAIN_PORT}/g" 02-internet.conf
+    sed -i "s/\[\[domain\]\]/${INTERNET_DOMAIN}/g" 02-internet.conf
     sed -i "s/\[\[enable_ssl\]\]/${ENABLE_SSL}/g" 02-internet.conf
-    printf "done\n"
-
-    printf "Writing Internet redirect file... "
-    cp stencils/02-http_redirect.conf ./includes/02-http_redirect.conf
-    sed -i "s/\[\[internet_domain\]\]/${INTERNET_DOMAIN}/g" ./includes/02-http_redirect.conf
     printf "done\n"
 
     # Get back out
     cd ..
 fi
 
-# Build the container from the nginx folder and tag it
+# Build the container from the NGINX folder and tag it
 TIMESTAMP=$(date +"%s")
 
 docker build \
     --tag omniport-nginx:${TIMESTAMP} \
     --tag omniport-nginx:latest \
     .
-
-# Remove the .conf files after they have served their purpose
-rm conf.d/01-intranet.conf
-rm conf.d/02-internet.conf
-rm conf.d/includes/01-http_redirect.conf
-rm conf.d/includes/02-http_redirect.conf
